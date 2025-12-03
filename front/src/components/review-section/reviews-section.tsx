6b49d78e22e37dde1a5e89fb83760a5cf1783eca
mindsetLuka <Roman.Khairutdinov@urfu.me>
2025-12-03 18:41:58 +0500

    поставил индикаторы и стрелки на место

--- front/src/components/review-section/reviews-section.tsx
+++ front/src/components/review-section/reviews-section.tsx
@@ -15,7 +15,7 @@ function ReviewsSection() {
         <section className="flex flex-col w-full py-[64px] max-w-[1280px]">
             <h2 className="text-center font-semibold text-[64px] text-[var(--color-dark-blue)] pb-[56px]">Отзывы</h2>
             <Carousel
-                className="*:overflow-visible!"
+                className="*:overflow-visible! "
                 showArrows={true}
                 showIndicators={true}
                 showThumbs={false}
@@ -32,7 +32,7 @@ function ReviewsSection() {
                         <button
                             onClick={onClickHandler}
                             title={label}
-                            className="absolute -left-15 group top-1/2 -translate-y-1/2 z-10 w-12 h-12 border border-[var(--color-blue)]/55 hover:border-[var(--color-blue)] rounded-full flex items-center justify-center shadow transition"
+                            className="absolute -left-12 group top-2/5 -translate-y-1/2 z-10 w-12 h-12 border border-[var(--color-blue)]/55 hover:border-[var(--color-blue)] rounded-full flex items-center justify-center shadow transition"
                         >
                             <svg fill="none" viewBox="0 0 24 24" strokeWidth="2" className="w-6 h-6 stroke-[var(--color-blue)]/55 group-hover:stroke-[var(--color-blue)] transition">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
@@ -45,7 +45,7 @@ function ReviewsSection() {
                         <button
                             onClick={onClickHandler}
                             title={label}
-                            className="absolute -right-15 group top-1/2 -translate-y-1/2 z-10 w-12 h-12 border border-[var(--color-blue)]/55 hover:border-[var(--color-blue)] rounded-full flex items-center justify-center shadow transition"
+                            className="absolute -right-12 group top-2/5 -translate-y-1/2 z-10 w-12 h-12 border border-[var(--color-blue)]/55 hover:border-[var(--color-blue)] rounded-full flex items-center justify-center shadow transition"
                         >
                             <svg fill="none" viewBox="0 0 24 24" strokeWidth="2" className="w-6 h-6 stroke-[var(--color-blue)]/55 group-hover:stroke-[var(--color-blue)] transition">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
@@ -54,7 +54,7 @@ function ReviewsSection() {
                     )
                 )}
                 renderIndicator={(onClickHandler, isSelected, index, label) => {
-                    const base = "inline-block w-3 h-3 mx-1 rounded-full cursor-pointer";
+                    const base = "inline-block w-2 h-2 mx-1 rounded-full cursor-pointer";
                     return (
                         <li
                             className={
@@ -72,7 +72,7 @@ function ReviewsSection() {
                 }}
             >
                 {reviews.map((item) => (
-                    <div key={item.id} className="px-4 h-full">
+                    <div key={item.id} className="border-b-[50px] border-b-[var(--color-beige)] px-4 h-full" >
                         <div className="pt-[24px] px-[24px] pb-[64px] bg-[var(--color-blue)]/15 rounded-lg shadow p-6 h-full flex flex-col text-left">
                             <span className="font-semibold text-lg text-[var(--color-blue)]">{item.author}</span>
                             <p className="text-lg text-[var(--color-blue)]/55 pb-[16px]">{item.company}</p>
